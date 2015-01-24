7f134815669c3834e94a6f678c820b8980bd9feb
cc <cc@cc-rmbp>
2015-01-24 12:35:01 -0500

    agregando cambios para el ahorcado

--- js/vg.js
+++ js/vg.js
@@ -3,12 +3,16 @@ function gameStart(){
      game= new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create:     create, update: update });
 }
 
+var hero = null;
+
+var a = new Ahorcado(hero); 
 
 function preload() {
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+	a.preload(game);
 }
 
 var player;
@@ -88,6 +92,7 @@ function create() {
 
     //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
+    a.create();
     
 }
 
@@ -130,6 +135,8 @@ function update() {
     {
         player.body.velocity.y = -350;
     }
+    
+    a.update();
 
 }
 
