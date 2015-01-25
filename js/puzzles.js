56fa7e785773ab1a713185b4bf4222c68667c2fe
Javier <javier.palomino@tektonlabs.com>
2015-01-25 16:26:26 -0500

    mensajes

--- js/puzzles.js
+++ js/puzzles.js
@@ -1,4 +1,3 @@
-
 var Index_Scenario	= 0;
 var Message 		= 1;
 var Answer 			= 2;
@@ -6,10 +5,10 @@ var Reward  		= 3;
 var Type 			= 4;
 
 var puzzles_map = [
-	[1,"Welcome","","",""],
-	[2,"Adivinanza 1","Respuesta 1","Premio 1",""],
-	[5,"Adivinanza 2","Respuesta 2","Premio 2",""],
-	[6,"Adivinanza 3","Respuesta 3","Premio 3",""],
-	[7,"Adivinanza 4","Respuesta 4","Premio 4",""],
-	[9,"Adivinanza 5","Respuesta 5","Premio 5",""]
+	[1,"Welcome and hope u die","","",""],
+	[2,"¿Cual es la sangre de los cerros?","Respuesta 1","Premio 1",""],
+	[5,"En una loma en las alturas, un viejo con \n larga barba blanca está tocando el violin\n ¿Quién es? ","Respuesta 2","Premio 2",""],
+	[6,"Por la mañana camina en cuatro,	al medio día \n en dos y por las tardes en tres\n ¿Qué será?","Respuesta 3","Premio 3",""],
+	[7,"En una pampa azul y silenciosa, están esparcidos\n los granos de maiz tostado. \n ¿Quiénes son?","Respuesta 4","Premio 4",""],
+	[9,"Sin ser rica tengo cuartos y, sin morir, nazco nueva;\n y a pesar de que no como,\n hay noches que luzco llena.\n ¿Quién soy?","Respuesta 5","Premio 5",""]
 ];
